b63a30d04b7f6a255199a0169229cd45d92d73a2
suleei <suleei@qq.com>
2024-08-14 17:26:17 +0800

    finish all functions and field verify in login page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,7 @@
   <div id="form-platform" style="overflow: hidden;display: flex;align-items: center">
     <div style="width: 100%">
       <div style="text-align: center;font-size: 1.5rem;color: grey;">OPEN FIT TOOLS</div>
+
       <v-divider style="margin-top: 5%"></v-divider>
       <v-btn variant="text" style="color: grey" rounded="0" @click="passwordLoginButtonClickHandler">
         密码登录
@@ -11,50 +12,207 @@
       <v-btn variant="text" style="color: grey" rounded="0" @click="messageLoginButtonClickHandler">
         短信登录
       </v-btn>
+      <v-btn variant="text" style="color: grey" rounded="0" @click="thirdPartyLoginButtonClickHandler">
+        第三方登录
+      </v-btn>
       <v-divider></v-divider>
-      <div style="height: 100%" v-show="loginDisplay">
+      <div style="height: 100%" v-if="displayStatus.thirdParty">
+        <div style="margin-top: 5%;height: 50%;">
+          <div style="display: flex;justify-content: space-between;width: 50%;margin: 10% auto">
+            <v-btn icon="mdi-qqchat" variant="outlined" style="color: grey" size="x-large"></v-btn>
+            <v-btn icon="mdi-wechat" variant="outlined" style="color: grey" size="x-large"></v-btn>
+            <v-btn icon="mdi-sina-weibo" variant="outlined" style="color: grey" size="x-large"></v-btn>
+          </div>
+          <div style="display: flex;justify-content: space-between;width: 50%;margin: 0 auto">
+            <v-btn icon="mdi-github" variant="outlined" style="color: grey" size="x-large"></v-btn>
+            <v-btn icon="mdi-apple" variant="outlined" style="color: grey" size="x-large"></v-btn>
+            <v-btn icon="mdi-android" variant="outlined" style="color: grey" size="x-large"></v-btn>
+          </div>
+        </div>
+      </div>
+      <div style="height: 100%" v-if="displayStatus.login">
         <div style="margin-top: 5%;height: 50%">
-          <v-text-field label="用户名" variant="outlined" id="username-input"></v-text-field>
-          <v-text-field label="密码" variant="outlined" id="password-input"></v-text-field>
+          <v-form v-model="status.formSubmitEnable">
+            <v-text-field
+              label="用户名"
+              variant="outlined"
+              prepend-inner-icon="mdi-account-outline"
+              v-model="formFields.username"
+              :rules="[rules.required, rules.username_size_limit]"
+              counter maxlength="20">
+            </v-text-field>
+            <v-text-field
+              :append-inner-icon="status.password_visible ? 'mdi-eye-off' : 'mdi-eye'"
+              :prepend-inner-icon="status.passwordReady ? 'mdi-lock-outline' : 'mdi-lock-open-variant-outline'"
+              :type="status.password_visible ? 'text' : 'password'"
+              label = "密码"
+              variant="outlined"
+              @click:append-inner="status.password_visible = !status.password_visible"
+              v-model="formFields.password"
+              :rules="[rules.required,rules.password]"
+              style="margin-top: 2%;">
+            </v-text-field>
+          </v-form>
+          <v-btn variant="text" style="color: grey;float: right" @click="retrieveButtonClickHandler">忘记密码</v-btn>
           <v-btn variant="text" style="color: grey;float: right" @click="registerButtonClickHandler">注册</v-btn>
-          <v-btn variant="outlined" id="login-button">
-            登录
-          </v-btn>
+          <v-btn variant="outlined" id="login-button" :disabled="!status.formSubmitEnable">登录</v-btn>
         </div>
       </div>
-      <div style="height: 100%" v-show="registerDisplay">
+
+      <div style="height: 100%" v-if="displayStatus.register">
         <div style="margin-top: 5%;height: 50%">
-          <v-text-field label="用户名" variant="outlined" id="username-input"></v-text-field>
-          <v-text-field label="密码" variant="outlined" id="password-input"></v-text-field>
-          <v-text-field label="确认密码" variant="outlined" id="password-check"></v-text-field>
-          <div style="width: 100%;display: flex">
-            <v-text-field label="邮箱" variant="outlined" id="password-check" style="width: 68%"></v-text-field>
-            <v-btn variant="outlined" style="width: 20%;height: 3.5rem;color: grey;margin-left: 2%">
-              发送验证码
-            </v-btn>
-          </div>
-          <v-text-field label="验证码" variant="outlined" id="password-check"></v-text-field>
-          <v-btn variant="text" style="color: grey;float: right" @click="returnLoginButtonClickHandler">返回登录</v-btn>
-          <v-btn variant="outlined" id="login-button">
-            注册
-          </v-btn>
+          <v-form v-model="status.formSubmitEnable">
+            <v-text-field
+              label="用户名"
+              variant="outlined"
+              prepend-inner-icon="mdi-account-outline"
+              v-model="formFields.username"
+              :rules="[rules.required, rules.username_size_limit]"
+              counter maxlength="20">
+            </v-text-field>
+            <v-text-field
+              :append-inner-icon="status.password_visible ? 'mdi-eye-off' : 'mdi-eye'"
+              :type="status.password_visible ? 'text' : 'password'"
+              label = "密码"
+              :prepend-inner-icon="status.passwordReady ? 'mdi-lock-outline' : 'mdi-lock-open-variant-outline'"
+              variant="outlined"
+              @click:append-inner="status.password_visible = !status.password_visible"
+              v-model="formFields.password"
+              :rules="[rules.required,rules.password]"
+              style="margin-top: 2%;">
+            </v-text-field>
+            <v-text-field
+              :append-inner-icon="status.password_check_visible ? 'mdi-eye-off' : 'mdi-eye'"
+              :type="status.password_check_visible ? 'text' : 'password'"
+              label = "确认密码"
+              :prepend-inner-icon="status.passwordCheckReady ? 'mdi-lock-check-outline' : 'mdi-lock-remove-outline'"
+              prepend-inner-icon="mdi-lock-check-outline"
+              variant="outlined"
+              @click:append-inner="status.password_check_visible = !status.password_check_visible"
+              v-model="formFields.passwordCheck"
+              :rules="[rules.required,rules.passwordCheck]"
+              style="margin-top: 2%;">
+            </v-text-field>
+            <div  style="width: 100%;display: flex;margin-top: 2%;">
+                <v-text-field
+                  label="邮箱"
+                  variant="outlined"
+                  id="password-check"
+                  style="width: 68%;"
+                  prepend-inner-icon="mdi-email-outline"
+                  v-model="formFields.eMail"
+                  :rules="[rules.required, rules.email]">
+                </v-text-field>
+                <v-btn variant="outlined" style="width: 20%;height: 3.5rem;color: grey;margin-left: 2%" :disabled="!(status.identifyingCodeSendingEnabled && !status.identifyingCodeSendingCooldown)" @click="identifyingCodeSendingButtonClickHandler">
+                  {{status.identifyingCodeSendingButtonText}}
+                </v-btn>
+            </div>
+            <v-text-field
+              label="验证码"
+              prepend-inner-icon="mdi-message-processing-outline"
+              variant="outlined"
+              v-model="formFields.identifyingCode"
+              :rules="[rules.required,rules.identifyingCode]"
+              style="margin-top: 2%">
+            </v-text-field>
+            <v-btn variant="text" style="color: grey;float: right" @click="returnLoginButtonClickHandler">返回登录</v-btn>
+            <v-btn variant="outlined" id="login-button" :disabled="!status.formSubmitEnable">注册</v-btn>
+          </v-form>
         </div>
       </div>
-      <div style="height: 100%" v-show="messageDisplay">
+
+      <div style="height: 100%" v-if="displayStatus.retrieve">
         <div style="margin-top: 5%;height: 50%">
-          <v-alert text="未注册手机验证后自动登录" variant="text" style="color: grey;height: 0"></v-alert>
+          <v-form v-model="status.formSubmitEnable">
+            <v-text-field
+              label="用户名"
+              variant="outlined"
+              prepend-inner-icon="mdi-account-outline"
+              v-model="formFields.username"
+              :rules="[rules.required, rules.username_size_limit]"
+              counter maxlength="20">
+            </v-text-field>
+            <div  style="width: 100%;display: flex;margin-top: 2%;">
+              <v-text-field
+                label="绑定邮箱"
+                variant="outlined"
+                id="password-check"
+                style="width: 68%;"
+                prepend-inner-icon="mdi-email-outline"
+                v-model="formFields.eMail"
+                :rules="[rules.required, rules.email]">
+              </v-text-field>
+              <v-btn variant="outlined" style="width: 20%;height: 3.5rem;color: grey;margin-left: 2%" :disabled="!(status.identifyingCodeSendingEnabled && !status.identifyingCodeSendingCooldown)" @click="identifyingCodeSendingButtonClickHandler">
+                {{status.identifyingCodeSendingButtonText}}
+              </v-btn>
+            </div>
+            <v-text-field
+              label="验证码"
+              prepend-inner-icon="mdi-message-processing-outline"
+              variant="outlined"
+              v-model="formFields.identifyingCode"
+              :rules="[rules.required,rules.identifyingCode]"
+              style="margin-top: 2%">
+            </v-text-field>
+            <v-text-field
+              :append-inner-icon="status.password_visible ? 'mdi-eye-off' : 'mdi-eye'"
+              :type="status.password_visible ? 'text' : 'password'"
+              label = "新密码"
+              :prepend-inner-icon="status.passwordReady ? 'mdi-lock-outline' : 'mdi-lock-open-variant-outline'"
+              variant="outlined"
+              @click:append-inner="status.password_visible = !status.password_visible"
+              v-model="formFields.password"
+              :rules="[rules.required,rules.password]"
+              style="margin-top: 2%;">
+            </v-text-field>
+            <v-text-field
+              :append-inner-icon="status.password_check_visible ? 'mdi-eye-off' : 'mdi-eye'"
+              :type="status.password_check_visible ? 'text' : 'password'"
+              label = "确认密码"
+              :prepend-inner-icon="status.passwordCheckReady ? 'mdi-lock-check-outline' : 'mdi-lock-remove-outline'"
+              prepend-inner-icon="mdi-lock-check-outline"
+              variant="outlined"
+              @click:append-inner="status.password_check_visible = !status.password_check_visible"
+              v-model="formFields.passwordCheck"
+              :rules="[rules.required,rules.passwordCheck]"
+              style="margin-top: 2%;">
+            </v-text-field>
+            <v-btn variant="text" style="color: grey;float: right" @click="returnLoginButtonClickHandler">返回登录</v-btn>
+            <v-btn variant="outlined" id="login-button" :disabled="!status.formSubmitEnable">找回密码</v-btn>
+          </v-form>
+        </div>
+      </div>
 
-          <div style="width: 100%;display: flex">
-            <v-text-field label="手机号" variant="outlined" id="password-check" style="width: 68%"></v-text-field>
-            <v-btn variant="outlined" style="width: 20%;height: 3.5rem;color: grey;margin-left: 2%">
-              发送验证码
+      <div style="height: 100%" v-if="displayStatus.message">
+        <div style="margin-top: 5%;height: 50%">
+          <v-form v-model="status.formSubmitEnable">
+            <div style="width: 100%;display: flex">
+              <v-text-field
+                label="手机号"
+                variant="outlined"
+                prepend-inner-icon="mdi-phone-outline"
+                id="password-check"
+                placeholder="未注册手机验证后自动登录"
+                v-model="formFields.phoneNumber"
+                :rules="[rules.required,rules.phoneNumber]"
+                style="width: 68%">
+              </v-text-field>
+              <v-btn variant="outlined" style="width: 20%;height: 3.5rem;color: grey;margin-left: 2%" :disabled="!(status.identifyingCodeSendingEnabled && !status.identifyingCodeSendingCooldown)" @click="identifyingCodeSendingButtonClickHandler">
+                {{status.identifyingCodeSendingButtonText}}
+              </v-btn>
+            </div>
+            <v-text-field
+              label="验证码"
+              prepend-inner-icon="mdi-message-processing-outline"
+              variant="outlined"
+              v-model="formFields.identifyingCode"
+              :rules="[rules.required,rules.identifyingCode]"
+              style="margin-top: 2%">
+            </v-text-field>
+            <v-btn variant="outlined" id="login-button" :disabled="!status.formSubmitEnable">
+              登录/注册
             </v-btn>
-          </div>
-          <v-text-field label="验证码" variant="outlined" id="password-check"></v-text-field>
-          <v-btn variant="outlined" id="login-button">
-            登录/注册
-          </v-btn>
-
+          </v-form>
         </div>
       </div>
     </div>
@@ -62,13 +220,10 @@
 </template>
 
 <script setup lang="js">
-import {onMounted, onUnmounted, ref} from "vue";
+import {onMounted, onUnmounted, reactive, ref} from "vue";
 import AMapLoader from "@amap/amap-jsapi-loader";
 
 let map = null;
-let loginDisplay = ref(true);
-let registerDisplay = ref(false);
-let messageDisplay = ref(false);
 onMounted(() => {
   window._AMapSecurityConfig = {
     securityJsCode: "c27ca67686a87d9e69ddf86ac03be353",
@@ -96,23 +251,143 @@ onUnmounted(() => {
   map?.destroy();
 });
 
+
+const displayStatus = reactive({
+  login: true,
+  register: false,
+  message: false,
+  retrieve: false,
+  thirdParty: false
+})
+
+const initialFormFields ={
+  username: "",
+  password: "",
+  passwordCheck: "",
+  eMail: "",
+  phoneNumber: "",
+  identifyingCode: ""
+}
+
+const initialStatus = {
+  formSubmitEnable:false,
+  email: '',
+  username: '',
+  password_visible: false,
+  password_check_visible: false,
+  identifyingCodeSendingEnabled: false,
+  identifyingCodeSendingCooldown: false,
+  identifyingCodeSendingButtonText: "发送验证码",
+  passwordReady:false,
+  passwordCheckReady:false
+}
+
+let formFields = reactive({
+  ...initialFormFields
+})
+
+let status = reactive({
+  ...initialStatus
+})
+
+let rules={
+  required: value => !!value || '必填字段',
+  username_size_limit: value => value.length <= 20 || '最大20个字符',
+  email: value => {
+    const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if(displayStatus.register){
+      if(pattern.test(value)===true) status.identifyingCodeSendingEnabled=true
+      else status.identifyingCodeSendingEnabled=false
+    }
+    return pattern.test(value) || '错误的邮箱格式'
+  },
+  identifyingCode: value=>{
+    const pattern = /\d{6}/
+    return pattern.test(value) || '错误的验证码格式'
+  },
+  password: value=>{
+    const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@!%*?&])[A-Za-z\d@!%*?&]{8,}$/
+    if(pattern.test(value)) status.passwordReady=true;
+    else status.passwordReady=false;
+    return pattern.test(value) || '大小写字母、数字和特殊字符各至少出现一次且长度大于8'
+  },
+  passwordCheck: value=>{
+    if(formFields.password===formFields.passwordCheck) status.passwordCheckReady=true;
+    else status.passwordCheckReady=false;
+    return (formFields.password===formFields.passwordCheck) || '两次输入的密码不一致'
+  },
+  phoneNumber: value=>{
+    const pattern = /1[3-9]\d{9}/
+    if(displayStatus.message){
+      if(pattern.test(value)===true) status.identifyingCodeSendingEnabled=true
+      else status.identifyingCodeSendingEnabled=false
+    }
+    return pattern.test(value) || '错误的电话号码格式'
+  },
+}
+
+function statusReset(){
+  formFields = reactive({
+    ...initialFormFields
+  })
+  status = reactive({
+    ...initialStatus
+  })
+}
 function registerButtonClickHandler(){
-  loginDisplay.value = false;
-  registerDisplay.value = true;
+  statusReset();
+  displayStatus.login = false;
+  displayStatus.register = true;
+}
+
+function retrieveButtonClickHandler(){
+  statusReset();
+  displayStatus.retrieve = true;
+  displayStatus.login = false;
 }
 function returnLoginButtonClickHandler(){
-  loginDisplay.value = true;
-  registerDisplay.value = false;
+  statusReset();
+  displayStatus.login = true;
+  displayStatus.register = false;
+  displayStatus.retrieve = false;
 }
 function passwordLoginButtonClickHandler(){
-  loginDisplay.value=true;
-  registerDisplay.value = false;
-  messageDisplay.value=false;
+  statusReset();
+  displayStatus.login = true;
+  displayStatus.register = false;
+  displayStatus.message = false;
+  displayStatus.retrieve = false;
+  displayStatus.thirdParty=false;
 }
 function messageLoginButtonClickHandler(){
-  loginDisplay.value=false;
-  registerDisplay.value = false;
-  messageDisplay.value=true;
+  statusReset();
+  displayStatus.login = false;
+  displayStatus.register = false;
+  displayStatus.retrieve = false;
+  displayStatus.message = true;
+  displayStatus.thirdParty=false;
+}
+function thirdPartyLoginButtonClickHandler(){
+  statusReset();
+  displayStatus.login = false;
+  displayStatus.register = false;
+  displayStatus.retrieve = false;
+  displayStatus.message = false;
+  displayStatus.thirdParty=true;
+}
+function identifyingCodeSendingButtonClickHandler(){
+  status.identifyingCodeSendingCooldown=true;
+  let counter = 6;
+  status.identifyingCodeSendingButtonText = counter + 's';
+  let countDowner = setInterval(()=>{
+    counter--;
+    status.identifyingCodeSendingButtonText = counter + 's';
+    if(counter===0) {
+      status.identifyingCodeSendingButtonText= "发送验证码"
+      status.identifyingCodeSendingCooldown=false;
+      clearInterval(countDowner)
+    }
+  },1000)
 }
 </script>
 <style  scoped>
